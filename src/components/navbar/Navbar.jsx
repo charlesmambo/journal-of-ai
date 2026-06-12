66acb20420657255899e566a95c1e28c20c2f126
unknown <chylah11st@gmail.com>
2026-06-12 22:44:02 +0200

    fix mobile navbar view styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../navbar/Navbar.css';
 import { IoBookOutline } from "react-icons/io5";
 import { NavLink } from 'react-router-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseCircleOutline } from "react-icons/io5";
+
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+const closeMenu = () => {
+  setMenuOpen(false);
+};
   return (
     <div className="navbar">
       <div className="navbar-container">
@@ -52,6 +60,50 @@ const Navbar = () => {
         </NavLink>
 
       </div>
+     <div className="hamburger">
+  {menuOpen ? (
+    <IoCloseCircleOutline
+      className="ham-icon close"
+      onClick={() => setMenuOpen(false)}
+    />
+  ) : (
+    <RxHamburgerMenu
+      className="ham-icon open"
+      onClick={() => setMenuOpen(true)}
+    />
+  )}
+</div>
+       <nav className={`nav-links mobile-nav-links ${menuOpen ? "show" : ""}`}>
+          <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>
+            About
+          </NavLink>
+
+          <NavLink to="/archive" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>
+            Archive
+          </NavLink>
+
+          <NavLink to="/submissions" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>
+            Submissions
+          </NavLink>
+
+          <NavLink to="/guidelines" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>
+            Guidelines
+          </NavLink>
+
+          <NavLink to="/editorial-board" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>
+            Editorial Board
+          </NavLink>
+
+          <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>
+            Contact
+          </NavLink>
+
+          {/* CTA Button */}
+        <NavLink to="/submissions" onClick={closeMenu}>
+        <button className="submit-btn mobile-submit-btn">Submit Paper</button>
+        </NavLink>
+        </nav>
+
     </div>
   )
 }
